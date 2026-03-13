78c6e7b39b1e0434792901f13e97b78703d5e466
swarajbabu <syamalaswaraj@gmail.com>
2026-03-13 21:11:16 +0530

    .

--- 7-EJS/index.js
+++ 7-EJS/index.js
@@ -2,6 +2,11 @@ const express = require("express")
 const app = express()
 const port = 8080;
 
+const path = require("path")
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"/views"))
+
+
 app.listen(port,()=>{
     console.log(`Server is Started in ${port}`)
 })
